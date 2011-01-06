020b8ce9889161a347258aafc65b036a144b84b7
Christophe Verbinnen <djpate@gmail.com>
2011-01-07 00:42:29 +0100

    fix

--- class/provider.class.php
+++ class/provider.class.php
@@ -9,16 +9,16 @@
 			$this->oauth = new OAuthProvider();
 			
 			/* setup check functions */
-			$this->consumerHandler(array($this,'checkConsumer'));
-			$this->timestampNonceHandler(array($this,'checkNonce'));
-			$this->tokenHandler(array($this,'checkToken'));
+			$this->oauth->consumerHandler(array($this,'checkConsumer'));
+			$this->oauth->timestampNonceHandler(array($this,'checkNonce'));
+			$this->oauth->tokenHandler(array($this,'checkToken'));
 			
 			/* Set the request token url so we dont run checkToken() on a RequestToken */
-			$this->setRequestTokenPath("/OAuthProviderExample/oauth/request_token");
+			$this->oauth->setRequestTokenPath("/OAuthProviderExample/oauth/request_token");
 			
 			/* now that everything is setup we run the checks */
 			try {
-				$this->checkOAuthRequest();
+				$this->oauth->checkOAuthRequest();
 			} catch(OAuthException $E){
 				echo OAuthProvider::reportProblem($E);
 				$this->oauth_error = true;
