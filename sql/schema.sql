927363eced302d2d612678ea2761de155781436f
Christophe Verbinnen <pate@christophe.(none)>
2011-01-12 16:46:43 +0100

    cleanup sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Mar 11 Janvier 2011 à 16:20
+-- Généré le : Mer 12 Janvier 2011 à 16:46
 -- Version du serveur: 5.1.49
 -- Version de PHP: 5.3.3-1ubuntu9.1
 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `consumer` (
   `consumer_secret` varchar(255) NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
 -- Contenu de la table `consumer`
@@ -47,20 +47,12 @@ CREATE TABLE IF NOT EXISTS `consumer_nonce` (
   `nonce` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `consumer_id` (`consumer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `consumer_nonce`
 --
 
-INSERT INTO `consumer_nonce` (`id`, `consumer_id`, `timestamp`, `nonce`) VALUES
-(1, 1, 1294757729, '2976154794d2c6f618bc483.23732359'),
-(2, 1, 1294757751, '10509176314d2c6f77ad4275.55071316'),
-(3, 1, 1294757838, '18660214244d2c6fcec0a1a7.65990300'),
-(4, 1, 1294758269, '11599848504d2c717d881e99.02414189'),
-(5, 1, 1294758336, '19224957604d2c71c0589d10.49233830'),
-(6, 1, 1294759020, '6687229184d2c746cc32bc5.31210714'),
-(7, 1, 1294759040, '10019149574d2c7480a2ffe7.01479224');
 
 -- --------------------------------------------------------
 
@@ -81,15 +73,12 @@ CREATE TABLE IF NOT EXISTS `token` (
   KEY `consumer_id` (`consumer_id`),
   KEY `user_id` (`user_id`),
   KEY `type` (`type`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `token`
 --
 
-INSERT INTO `token` (`id`, `type`, `consumer_id`, `user_id`, `token`, `token_secret`, `callback_url`, `verifier`) VALUES
-(1, 1, 1, 1, '06197f5330f83fdce9942634a2da74e126bb6db5', 'b13bcba3adf8d11265122e84ab4d6a2c984bb522', 'http://localhost/OAuthProviderExample/client/callback.php', '01c5b4d116b93503892c4eef8a9beb2903785f10'),
-(2, 1, 1, 1, 'c7732f64edf63cba8c11426cfd06306bdb9a2d05', '4b67e09bd13e57695a5c37603d20088613864878', 'http://localhost/OAuthProviderExample/client/callback.php', '632e0db933a2dfe9621ee6fe0dade187e311b061');
 
 -- --------------------------------------------------------
 
@@ -146,4 +135,3 @@ ALTER TABLE `consumer_nonce`
 ALTER TABLE `token`
   ADD CONSTRAINT `token_ibfk_2` FOREIGN KEY (`type`) REFERENCES `token_type` (`id`),
   ADD CONSTRAINT `token_ibfk_1` FOREIGN KEY (`consumer_id`) REFERENCES `consumer` (`id`) ON DELETE CASCADE;
-
