5f39e4ad23e21721e378a208bf91d63939668db9
Christophe Verbinnen <djpate@gmail.com>
2011-05-31 07:27:14 -0700

    Fix according to http://tools.ietf.org/html/rfc5849#section-2.2

--- client/callback.php
+++ client/callback.php
@@ -1,11 +1,11 @@
 <?php
-	if(isset($_REQUEST['oauth_token'])&&isset($_REQUEST['verifier_token'])){
+	if(isset($_REQUEST['oauth_token'])&&isset($_REQUEST['oauth_verifier'])){
 		if(isset($_POST['oauth_token'])){
 			try{
 				$oauth_client = new Oauth("key","secret");
 				$oauth_client->enableDebug();
 				$oauth_client->setToken($_POST['oauth_token'],$_POST['oauth_token_secret']);
-				$info = $oauth_client->getAccessToken("http://localhost/OAuthProviderExample/oauth/access_token",null,$_POST['verifier_token']);
+				$info = $oauth_client->getAccessToken("http://localhost/OAuthProviderExample/oauth/access_token",null,$_POST['oauth_verifier']);
 				echo "<h1>Congrats !</h1>";
 				echo "<strong>AccessToken</strong> ".$info['oauth_token']."<br />";
 				echo "<strong>AccessToken Secret</strong> ".$info['oauth_token_secret'];
